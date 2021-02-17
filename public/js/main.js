10b5b8ec139adb1c884b30743d3d93846b9e28d5
pannu28 <Shubhampannu7@gmail.com>
2021-02-18 00:28:42 +0530

    add lang snippets

--- public/js/main.js
+++ public/js/main.js
@@ -8,16 +8,13 @@ let ts=document.getElementById("ts");
 
 function click(){
      sub.innerHTML="running..";
-     // out.style.display="block";
-     // time.style.display="block";
-     // status.style.display="block";
-     // ts.style.display="block";
 }
 
 let butn=document.getElementsByClassName("btn");
 
 for(var i=0;i<butn.length;i++){
      butn[i].addEventListener("click",lang);
+     butn[i].addEventListener("click",act);
 }
 
 function lang(){
@@ -26,21 +23,127 @@ function lang(){
      }
 }
 
-let c=document.getElementById("c");
-let cpp=document.getElementById("cpp");
-let java=document.getElementById("java");
-let python=document.getElementById("python");
-let javascript=document.getElementById("javascript");
-
-c.addEventListener("click",act);
-java.addEventListener("click",act);
-cpp.addEventListener("click",act);
-javascript.addEventListener("click",act);
-python.addEventListener("click",act);
+let codes={
+       "C": '#include <stdio.h>\n\nint main() {\n     //code\n     return 0;\n}',
+     "CPP14": '#include <iostream>\nusing namespace std;\n\nint main() {\n    cout<<"YoYaPa!";\n    return 0;\n}',
+     "JAVA8":'/*package whatever //do not write package name here */\n\nimport java.io.*;\n\nclass YoYaPa {\n    public static void main (String[] args) {\n        System.out.println("YoYaPa!");\n   }\n}',
+     "PYTHON3": '#code\nprint("YoYaPa")',
+     "CPP": '#include <iostream>\nusing namespace std;\n\nint main() {\n   cout<<"YoYaPa!";\n   return 0;\n}'
+  }; 
 
 let txt=document.getElementById("text");
 
 function act(){
        this.classList.add("active");
        txt.value=this.innerHTML;
+       localStorage.setItem("lang",this.innerHTML);
+
+       let c=localStorage.getItem(this.innerHTML);
+       if(c===undefined||c===null||c===""){
+          rest();
+       }else{
+              document.getElementById("code").value=c;
+       }
+}
+
+let copy=document.getElementById("copy2");
+copy.addEventListener("click",cpy);
+
+function cpy(){
+     let text=document.getElementById("code");
+     text.select();
+  document.execCommand('copy');
+
+  copy.innerHTML="Copied";
+  setTimeout(function(){
+     copy.innerHTML="COPY";
+  },1000);
+}
+
+let incopy=document.getElementById("incopy");
+incopy.addEventListener("click",incpy);
+
+function incpy(){
+     let text=document.getElementById("input");
+     text.select();
+  document.execCommand('copy');
+
+  incopy.value="Copied";
+  incopy.style.right="4px";
+  setTimeout(function(){
+     incopy.value="COPY";
+     incopy.style.right="8px";
+  },1000);
+}
+
+
+let outcopy=document.getElementById("outcopy");
+outcopy.addEventListener("click",outcpy);
+
+function outcpy(){
+     let text=document.getElementById("output");
+     text.select();
+  document.execCommand('copy');
+
+  outcopy.value="Copied";
+  outcopy.style.right="4px";
+  setTimeout(function(){
+     outcopy.value="COPY";
+     outcopy.style.right="8px";
+  },1000);
+  
+}
+
+let code=document.getElementById("code");
+code.addEventListener("change",change);
+
+  function change(){
+       let lan =document.getElementsByClassName("active");
+       localStorage.setItem(lan[0].innerHTML,code.value);
+  }
+
+
+function load(){
+       
+     let cl=document.getElementById(localStorage.getItem("lang"));
+     let c=localStorage.getItem(cl.innerHTML);
+     if(c===undefined||c===null||c===""){
+          rest();
+       }else{
+              document.getElementById("code").value=c;
+       }
+ 
+     for(var i=0;i<butn.length;i++){
+          butn[i].classList.remove("active");
+     }
+     cl.classList.add("active");
+}
+
+let reset=document.getElementById("reset1");
+reset.addEventListener("click",rest);
+reset.addEventListener("click",set);
+
+function set() {
+     reset.innerHTML="DONE!";
+  setTimeout(function(){
+     reset.innerHTML="RESET";
+  },1000);
+}
+
+function  rest() {
+      let cl=document.getElementById(localStorage.getItem("lang"));
+      let val=cl.innerText;
+      let put;
+      if(val==="C"){
+      put=codes.C;
+      }else if(val==="CPP"||val==="CPP14"){
+       put=codes.CPP;
+      }else if(val==="PYTHON3"){
+        put=codes.PYTHON3;
+      }else{
+        put=codes.JAVA8;
+      }
+      document.getElementById("code").value=put;
+      localStorage.setItem(val,put);
+
 }
